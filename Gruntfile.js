68fc918231215421577b2a96efe2f65723152b28
DongXin9 <2627405095@qq.com>
2020-03-04 17:04:30 +0800

    imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,16 @@
 /* global module : true*/
 module.exports = function (grunt) {
   grunt.initConfig({
-  htmlmin: {
-      options: {
-        collapseWhitespace: true,
-        preserveLineBreaks: false
-      },
-      files: {
-        src: './index.html',
-        dest: 'dist/index.html'
-      }
-    } 
+  imagemin: {
+    files:{
+      expand:true,
+      src:['images/*.{png,jpg,gif}'],
+      dest:'dist/'
+    }    
+  } 
   });
 
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
  
-  grunt.registerTask('default', ['htmlmin']);
+  grunt.registerTask('default', ['imagemin']);
 };
