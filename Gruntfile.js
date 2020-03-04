b16d711caf558e66a68470f998144929de67b00e
DongXin9 <2627405095@qq.com>
2020-03-04 18:42:45 +0800

    customize

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,6 @@
 /* global module : true*/
-module.exports = function (grunt) {
-  grunt.initConfig({
-  sprite: {
-    files:{
-      src:['images/*.png'],
-      dest:'dist/bundle.png',
-      destCss:'dist/sprite.css'
-    }  
-  } 
+module.exports = function (grunt) { 
+  grunt.registerTask('build', 'build task',function(){
+    console.log('build task!')
   });
-
-  grunt.loadNpmTasks('grunt-spritesmith');
- 
-  grunt.registerTask('default', ['sprite']);
 };
